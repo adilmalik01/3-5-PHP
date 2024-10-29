215d057354db996e4276409b0da34d6fc5ac7998
adilmalik01 <malikadil5679@gmail.com>
2024-10-29 15:58:34 +0500

    complete delete operation

--- blogwithdatabase/createBlog.php
+++ blogwithdatabase/createBlog.php
@@ -1,19 +1,6 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "blogging_website";
-
-
-$conn = new mysqli($servername, $username, $password, $database);
-
-if ($conn->connect_error) {
-    die("" . $conn->connect_error);
-};
-
-
-
+include 'db.php';
 
 
 
--- blogwithdatabase/index.php
+++ blogwithdatabase/index.php
@@ -1,21 +1,24 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "blogging_website";
-
-
-$conn = new mysqli($servername, $username, $password, $database);
-
-if ($conn->connect_error) {
-    die("" . $conn->connect_error);
-};
-
+include  'db.php';
 
 
 $sql = "SELECT * FROM blogs";
 $result = mysqli_query($conn, $sql);
+
+
+
+if (isset($_GET["id"])) {
+    $id = $_GET["id"];
+
+    $sql = "DELETE FROM blogs WHERE id = $id";
+    $del_result = mysqli_query($conn, $sql);
+}
+
+
+
+
+
 
 $conn->close();
 
@@ -38,10 +41,20 @@ $conn->close();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Forms || PHP</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <style>
         .main {
             display: flex;
             flex-wrap: wrap;
+        }
+
+        .icon-div {
+            display: flex;
+            justify-content: end;
+            gap: 20px;
+            align-items: center;
+            flex-direction: row-reverse;
+            font-size: 20px;
         }
     </style>
 </head>
@@ -52,7 +65,7 @@ $conn->close();
     <?php include 'navbar.php' ?>
 
 
-    
+
     <h1 class="mt-4 ms-5">All Blogs</h1>
 
 
@@ -63,10 +76,13 @@ $conn->close();
 
             <div class="card mb-3">
                 <div class="card-body">
-                     
-                    <h5 class="card-title"> <?php  echo $post["title"] ?></h5>
-                    <p class="card-text"><?php  echo $post["content"] ?></p>
-                    <p class="card-text"><small class="text-body-secondary"><?php  echo $post["author_name"] ?></small></p>
+                    <h5 class="card-title"> <?php echo $post["title"] ?></h5>
+                    <p class="card-text"><?php echo $post["content"] ?></p>
+                    <p class="card-text"><small class="text-body-secondary"><?php echo $post["author_name"] ?></small></p>
+                    <div class="icon-div">
+                        <a href="index.php?id=<?php echo $post["id"] ?>"><i class="bi bi-trash-fill"></i></a>
+                        <a href=""><i class="bi bi-pencil-fill"></i></a>
+                    </div>
                 </div>
             </div>
 
